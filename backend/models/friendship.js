5c4e1bba83434ed31fd7f7c2cbf87350f7fd8d8c
rohit saini <rohit1322saini@gmail.com>
2025-04-09 06:34:50 +0530

    chat and friendship .js

---
+++ backend/models/friendship.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+const userSchema = new mongoose.Schema({
+    userId: { type: String, required: true },
+    friendId: { type: String, required: true },
+    status: { type: String, enum: ["pending", "accepted", "rejected"], default: "pending" },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+    isDeleted: { type: Boolean, default: false },
+    deletedAt: { type: Date, default: null },
+    });
+
+export default mongoose.model('friendships', userSchema);
