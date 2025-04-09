4c7c696622d4b29d39f48287cc1fbd5e6fe8bf79
rohit saini <rohit1322saini@gmail.com>
2025-04-09 21:36:53 +0530

    server url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const server_url = import.meta.env.VITE_API_URI // || "https://dating-app-suvb.onrender.com/";
+export const server_url = import.meta.env.VITE_API_URI  || "https://dating-app-suvb.onrender.com/";
 export const static_url = import.meta.env.VITE_STATIC_URI || "https://pub-99e7d5ea040a44eeba75ec64a368f73b.r2.dev/";
 console.log("Server URL:", server_url);
