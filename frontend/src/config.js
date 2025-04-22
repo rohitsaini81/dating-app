50a9885ca2700fe5c1a49ccfa11c3f28d14c6f70
Rohit Saini <70269264+rohitsaini81@users.noreply.github.com>
2025-04-22 15:45:36 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const server_url = import.meta.env.VITE_API_URI  || "https://www.dating-app.xxxvideoss.site/";
+export const server_url = import.meta.env.VITE_API_URI  || "https://www.stream.xxxvideoss.site/";
 export const static_url = import.meta.env.VITE_STATIC_URI || "https://pub-99e7d5ea040a44eeba75ec64a368f73b.r2.dev/";
 console.log("Server URL:", server_url);
